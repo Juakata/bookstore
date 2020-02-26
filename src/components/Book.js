10578b509b265b150ee7d577dee127dc1bf8cc9f
Juakata <ha.uzquiano@hotmail.com>
2020-02-26 11:23:05 -0600

    add header and books style

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book, onClick }) => (
+const Book = ({ book, onClick, i }) => (
   <tr>
-    <td>{book.id}</td>
-    <td>{book.title}</td>
-    <td>{book.category}</td>
-    <td><button type="button" onClick={onClick}>Delete</button></td>
+    <td colSpan="4">
+      <div id={i === 0 ? 'first-book' : ''} className="book-tr">
+        <span>{book.category}</span>
+        <h2>{book.title}</h2>
+        <button type="button" onClick={onClick}>Delete</button>
+      </div>
+    </td>
   </tr>
 );
 
 Book.propTypes = {
   book: PropTypes.instanceOf(Object).isRequired,
   onClick: PropTypes.func.isRequired,
+  i: PropTypes.number.isRequired,
 };
 
 export default Book;
